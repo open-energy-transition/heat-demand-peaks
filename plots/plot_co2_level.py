28dd66074c2c11e1ef9e19b0ed69574f3b38c36c
yerbol-akhmetov <yerbol.akhmetov.93@gmail.com>
2024-09-16 22:34:25 +0500

    update names for scenarios in co2 plot

--- plots/plot_co2_level.py
+++ plots/plot_co2_level.py
@@ -228,9 +228,9 @@ def plot_co2_balance(co2_df, clusters, planning_horizon, plot_width=7):
     ax.set_ylim([-3.6,3.6])
 
     x_ticks = list(co2_df.columns)
-    if planning_horizon in ["2040", "2050"] and "Limited \nRenovation &\nOptimal Heating" in x_ticks:
+    if planning_horizon in ["2040", "2050"] and "Limited \nRenovation" in x_ticks:
         # replace name for Limited Renovation scenario for 2030 to be LROH
-        x_ticks[x_ticks.index("Limited \nRenovation &\nCost-Optimal Heating")] = "Limited \nRenovation &\nElectric Heating"
+        x_ticks[x_ticks.index("Limited \nRenovation")] = "Limited \nRenovation &\nElectrification"
 
     ax.set_xticklabels(x_ticks)
 
@@ -241,7 +241,7 @@ def plot_co2_balance(co2_df, clusters, planning_horizon, plot_width=7):
     )
 
     if planning_horizon == BAU_HORIZON:
-        ax.set_title("BAU", fontsize=12)
+        ax.set_title("2023", fontsize=12)
     else:
         ax.set_title(planning_horizon, fontsize=12)
 
@@ -323,10 +323,10 @@ if __name__ == "__main__":
     planning_horizons = [str(x) for x in planning_horizons if not str(x) == BAU_HORIZON]
 
     # define scenario namings
-    scenarios = {"flexible": "Optimal \nRenovation &\nCost-Optimal Heating", 
-                "retro_tes": "Optimal \nRenovation &\nElectric Heating", 
-                "flexible-moderate": "Limited \nRenovation &\nCost-Optimal Heating", 
-                "rigid": "No \nRenovation &\nElectric Heating"}
+    scenarios = {"flexible": "Widespread \nRenovation",
+                "retro_tes": "Widespread \nRenovation &\nElectrification",
+                "flexible-moderate": "Limited \nRenovation",
+                "rigid": "Business\nas Usual &\nElectrification"}
 
 
     # initialize df for storing co2 balance information
@@ -386,11 +386,11 @@ if __name__ == "__main__":
         print(f"Network is not found for scenario '{scenario}', planning year '{BAU_horizon}'. Skipping...")
     else:
         # get co2 balance for BAU and group technologies
-        co2_BAU = get_co2_balance(n, "BAU")
+        co2_BAU = get_co2_balance(n, "BASE 2023")
         co2_BAU = co2_BAU.groupby(co2_BAU.index.map(rename_techs)).sum()
         if not table_co2_df.empty and not co2_BAU.empty:
             plot_co2_balance(co2_BAU, clusters, BAU_horizon, plot_width=1.5)
-            table_co2_df = fill_table_df(table_co2_df, BAU_horizon, {"BAU":"BAU"}, co2_BAU)
+            table_co2_df = fill_table_df(table_co2_df, BAU_horizon, {"BAU":"BASE 2023"}, co2_BAU)
             table_co2_df = table_co2_df.fillna(0)
             co2_emission_BAU = -co2_BAU.loc["co2"].values[0]
 
@@ -409,18 +409,18 @@ if __name__ == "__main__":
         # save to csv
         table_co2_df.index.name = "CO2 emissions [tCO2_eq]"
         table_co2_df.columns = replace_multiindex_values(table_co2_df.columns, 
-                                                         ("2040", "Limited \nRenovation &\nCost-Optimal Heating"),
-                                                         ("2040","Limited \nRenovation &\nElectric Heating"))
+                                                         ("2040", "Limited \nRenovation"),
+                                                         ("2040","Limited \nRenovation &\nElectrification"))
         table_co2_df.columns = replace_multiindex_values(table_co2_df.columns, 
-                                                         ("2050", "Limited \nRenovation &\nCost-Optimal Heating"),
-                                                         ("2050","Limited \nRenovation &\nElectric Heating"))
+                                                         ("2050", "Limited \nRenovation"),
+                                                         ("2050","Limited \nRenovation &\nElectrification"))
         table_co2_df.to_csv(snakemake.output.table)
 
     if not co2_savings_df.empty:
         co2_savings_df.columns = replace_multiindex_values(co2_savings_df.columns, 
-                                                           ("2040", "Limited \nRenovation &\nCost-Optimal Heating"),
-                                                           ("2040","Limited \nRenovation &\nElectric Heating"))
+                                                           ("2040", "Limited \nRenovation"),
+                                                           ("2040","Limited \nRenovation &\nElectrification"))
         co2_savings_df.columns = replace_multiindex_values(co2_savings_df.columns, 
-                                                           ("2050", "Limited \nRenovation &\nCost-Optimal Heating"),
-                                                           ("2050","Limited \nRenovation &\nElectric Heating"))
+                                                           ("2050", "Limited \nRenovation"),
+                                                           ("2050","Limited \nRenovation &\nElectrification"))
         co2_savings_df.to_csv(snakemake.output.table_savings)
